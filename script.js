8923efa4576c06c9b59e8e2280dc0b54a4c489dd
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-18 12:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
 // your code here
+let Series = [];
+
+	for (let index = 0; index < num; index++) {
+		if(index == 0){
+			Series(index) = 0;
+		}else if(index == 1){
+			Series(index) = 1;
+		} else {
+		    Series(index)= series(index-1) + Series(index-2 );
+		}
+		
+	} 
+	return series; 
+
+
+	
 }
+
+
+
+
 
 module.exports = fibonacci;
